47d242e48824c8377369f99da92640f79b03e089
lomraig <lomraig@gmail.com>
2026-09-13 14:39:50 +0200

    asdf

--- shared/.config/fish/config.fish
+++ shared/.config/fish/config.fish
@@ -7,34 +7,6 @@ set -l os (uname)
 
 if test "$os" = Darwin
     eval "$(/opt/homebrew/bin/brew shellenv)"
-
-    ## pomodoro timer
-    function pom
-        set split $POMO_SPLIT
-        if ! test -n "$split"
-            set split $(gum choose "25/5" "50/10" "all done" --header "Choose a pomodoro split.")
-        end
-
-        switch $split
-            case 25/5
-                set work 25m
-                set break 5m
-            case 50/10
-                set work 50m
-                set break 10m
-            case 'all done'
-                return
-        end
-
-        timer $work && terminal-notifier -message Pomodoro \
-            -title 'Work Timer is up! Take a Break 😊' \
-            -sound Crystal
-
-        gum confirm "Ready for a break?" && timer $break && terminal-notifier -message Pomodoro \
-                -title 'Break is over! Get back to work 😬' \
-            -sound Crystal \
-            || pom
-    end
 else if test "$os" = Linux
     if status is-login
         if test (tty) = /dev/tty1
@@ -50,7 +22,7 @@ end
 #     end
 # end
 
-fish_vi_key_bindings # adds vim style movement
+# fish_vi_key_bindings # adds vim style movement
 
 set -gx EDITOR nvim
 set -g mouse on
